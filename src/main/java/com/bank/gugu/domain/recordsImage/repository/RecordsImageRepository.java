1d35a281334fa2052b631abad14e8a756cc1cfc5
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-06-26 20:23:40 +0900

    feat : 입/출금 내역 이미지 파일 등록, 삭제 API 작업

--- src/main/java/com/bank/gugu/domain/recordsImage/repository/RecordsImageRepository.java
+++ src/main/java/com/bank/gugu/domain/recordsImage/repository/RecordsImageRepository.java
@@ -1,7 +1,15 @@
 package com.bank.gugu.domain.recordsImage.repository;
 
+import com.bank.gugu.entity.common.constant.StatusType;
+import com.bank.gugu.entity.records.Records;
 import com.bank.gugu.entity.recordsImage.RecordsImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface RecordsImageRepository extends JpaRepository<RecordsImage, Long> {
+    List<RecordsImage> findAllByRecordsAndStatus(Records findRecord, StatusType statusType);
+
+    Optional<RecordsImage> findByIdAndStatus(Long id, StatusType statusType);
 }
--- src/main/java/com/bank/gugu/domain/recordsImage/service/RecordsImageService.java
+++ src/main/java/com/bank/gugu/domain/recordsImage/service/RecordsImageService.java
@@ -1,4 +1,21 @@
 package com.bank.gugu.domain.recordsImage.service;
 
+import com.bank.gugu.entity.user.User;
+import org.springframework.web.multipart.MultipartFile;
+
 public interface RecordsImageService {
+
+    /**
+     * 입/출금 내역 이미지 삭제
+     * @param recordsImageId 이미지 식별자
+     */
+    void deleteRecordImage(Long recordsImageId);
+
+    /**
+     * 입/출금 내역 이미지 등록
+     * @param recordsId 입/출금 식별자
+     * @param inputFile 등록 요청 파일 객체
+     * @param user 로그인 회원 식별자
+     */
+    void addRecordImage(Long recordsId, MultipartFile inputFile, User user);
 }
