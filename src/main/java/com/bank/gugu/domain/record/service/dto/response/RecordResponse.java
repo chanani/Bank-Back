8408ff01a89e90e299a6ddc6ecdb5e6fc1919fdc
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-06-26 20:25:20 +0900

    feat : 메인 페이지 달력 입/출금 내역 조회 API 작업

---
+++ src/main/java/com/bank/gugu/domain/record/service/dto/response/RecordResponse.java
@@ -0,0 +1,53 @@
+package com.bank.gugu.domain.record.service.dto.response;
+
+import com.bank.gugu.entity.common.constant.PriceType;
+import com.bank.gugu.entity.common.constant.RecordType;
+import com.bank.gugu.entity.records.Records;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+public class RecordResponse {
+
+    @Schema(description = "입/출금 내역 ID")
+    private Long id;
+
+    @Schema(description = "카테고리 아이콘")
+    private String categoryIcon;
+
+    @Schema(description = "타입")
+    private RecordType type;
+
+    @Schema(description = "금액")
+    private Integer price;
+
+    @Schema(description = "결제 유형")
+    private PriceType priceType;
+
+    @Schema(description = "할부")
+    private Integer monthly;
+
+    @Schema(description = "메모")
+    private String memo;
+
+    @Schema(description = "날짜")
+    private LocalDate userDate;
+
+    @Schema(description = "이미지 목록")
+    private List<RecordImagesResponse> recordImages;
+
+    public RecordResponse(Records record, List<RecordImagesResponse> images) {
+        this.id = record.getId();
+        this.categoryIcon = record.getCategory().getIcon().getPath();
+        this.type = record.getType();
+        this.price = record.getPrice();
+        this.priceType = record.getPriceType();
+        this.monthly = record.getMonthly();
+        this.memo = record.getMemo();
+        this.userDate = record.getUseDate();
+        this.recordImages = images;
+    }
+}
