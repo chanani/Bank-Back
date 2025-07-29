512d21ec572e1710a29d856c6330ef983e5b7d98
CHANHAN <130114269+chanani@users.noreply.github.com>
2025-07-29 18:00:57 +0900

    chore : ErrorAdviceHandler 내부 로직 수정

--- src/main/java/com/bank/gugu/global/exception/dto/ErrorResponse.java
+++ src/main/java/com/bank/gugu/global/exception/dto/ErrorResponse.java
@@ -1,9 +1,17 @@
 package com.bank.gugu.global.exception.dto;
 
-public record ErrorResponse(int code, int status, String message) {
+public record ErrorResponse(
+        int code,
+        int status,
+        String message
+) {
 
     public static ErrorResponse error(ErrorCode errorCode) {
-        return new ErrorResponse(errorCode.getCode(), errorCode.getStatus(), errorCode.getMessage());
+        return new ErrorResponse(
+                errorCode.getCode(),
+                errorCode.getStatus(),
+                errorCode.getMessage()
+        );
     }
 
     public static ErrorResponse error(int status, String message) {
